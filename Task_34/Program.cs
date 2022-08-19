24ecac2db2822da04d9185f8f08bb3e0d869a801
Denis.Karev <goofy.adun@gmail.com>
2022-08-19 16:14:41 +0300

    task 34 count even numbers in array

---
+++ Task_34/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными
+// трёхзначными числами. Напишите программу, которая покажет
+// количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write(array[i] + ", ");
+    }
+    Console.Write(array[array.Length - 1]);
+    Console.Write("]");
+}
+
+int CountEven3DigitsArrays (int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] %2 == 0) count++;
+    }
+    return count;
+}
+
+// Console.WriteLine("Введите размер массива:");
+// int size = Convert.ToInt32(Console.ReadLine());
+int size = 9;
+int[] arr123 = CreateArrayRndInt(size, 100, 1000);
+PrintArray(arr123);
+int evenElems = CountEven3DigitsArrays (arr123);
+Console.WriteLine($" -> {evenElems}");
